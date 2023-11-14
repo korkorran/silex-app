686b14fd33bae07aad8deecd586667a2a67ff092
Frédéric Lang <f.lang@hotmail.fr>
2023-11-14 13:36:13 +0100

    first commit

---
+++ src/Canva.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Stage, Layer, Star, Text, Image } from 'react-konva';
+import { File } from './models/file';
+import useImage from 'use-image';
+import fileImgUrl from './assets/file.png';
+
+function generateShapes() {
+  return [...Array(10)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
+
+const file : File = {
+  name: 'index.js'
+}
+
+const Canva = () => {
+  const [stars, setStars] = React.useState(INITIAL_STATE);
+
+  const handleDragStart = (e) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
+  const [image] = useImage(fileImgUrl)
+
+  return (
+    <Stage width={window.innerWidth - 15} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
+        <Image
+          image={image}
+          x={100}
+          y={100}
+          draggable
+          />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default Canva;
---
+++ src/models/file.ts
@@ -0,0 +1,3 @@
+export type File = {
+    name : string;
+}
