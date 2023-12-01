931d627c0962679e0c548c7ffc4bf3cdb66a5dac
Frédéric Lang <f.lang@hotmail.fr>
2023-12-01 11:54:13 +0100

    active tab highlight

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -3,11 +3,12 @@ import "./NavBar.scss"
 import { FaStar, FaChevronLeft, FaBars, FaPaste } from 'react-icons/fa';
 import { SlLink } from "react-icons/sl";
 import { RxColorWheel } from "react-icons/rx";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function NavBar() {
     const [toggled, setToggled] = useState(true);
     const navigate = useNavigate();
+    const {pathname} = useLocation()
     return (
         <div className="nav-bar">
             <input id="nav-toggle" type="checkbox" />
@@ -22,23 +23,32 @@ function NavBar() {
             </div>
             <hr/>
             <div className="nav-content">
-                <div className="nav-button" onClick={() => navigate("drag-a-star")}>
+                <div 
+                  className={"nav-button"+ (pathname === '/drag-a-star' ? ' active' : '') } 
+                  onClick={() => navigate("drag-a-star")}>
                     <FaStar />
                     { toggled && <span>Drag a Star</span> }
                 </div>
-                <div className="nav-button" onClick={() => navigate("wheel-of-fortune")}>
+                <div 
+                  className={"nav-button"+ (pathname === '/wheel-of-fortune' ? ' active' : '') } 
+                  onClick={() => navigate("wheel-of-fortune")}>
                     <RxColorWheel />
                     { toggled && <span>Wheel of Fortune</span> }
                 </div>
-                <div className="nav-button" onClick={() => navigate("post-it")}>
+                <div 
+                  className={"nav-button"+ (pathname === '/post-it' ? ' active' : '') }
+                  onClick={() => navigate("post-it")}>
                     <FaPaste />
                     { toggled && <span>Post-It</span>}
                 </div>
-                <div className="nav-button" onClick={() => navigate("connect-circles")}>
+                <div 
+                  className={"nav-button"+ (pathname === '/connect-circles' ? ' active' : '') } 
+                  onClick={() => navigate("connect-circles")}>
                     <SlLink />
                     { toggled && <span>Connect Circles</span>}
                 </div>
                 <div id="nav-content-highlight"></div>
+                <div id="nav-content-active"></div>
             </div>
 {/*             <hr/>
             <div className="nav-footer">
