992803912ff3f556643640ed9f2f1d6b5dd5b7ee
Frédéric Lang <f.lang@hotmail.fr>
2023-12-06 14:49:20 +0100

    init Giscus

--- src/demos/drag-a-star/DragAStar.tsx
+++ src/demos/drag-a-star/DragAStar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Stage, Layer, Star} from 'react-konva';
 import { FaGithub } from "react-icons/fa";
+import Giscus from '@giscus/react';
 /* import useImage from 'use-image'; 
 import fileImgUrl from '../../assets/file.png'; */
 
 function generateShapes() {
   return [...Array(10)].map((_, i) => ({
     id: 'target-' + i.toString(),
-    x: Math.random() * (window.innerWidth - 280),
+    x: Math.random() * (window.innerWidth - 300),
     y: Math.random() * window.innerHeight - 100,
     rotation: Math.random() * 180,
     isDragging: false,
@@ -46,7 +47,7 @@ const DragAStar = () => {
 
   return (
     <>
-    <Stage width={window.innerWidth - 280} height={window.innerHeight - 100}>
+    <Stage width={window.innerWidth - 300} height={window.innerHeight - 100}>
       <Layer>
         {stars.map((star) => (
           <Star
@@ -81,6 +82,21 @@ const DragAStar = () => {
       </Layer>
     </Stage>
     <p><b>Instructions : </b> Drag a star and see dragging effect. <a href='https://github.com/frederic-lang/awesome-konva-react-demos/blob/master/src/demos/drag-a-star/DragAStar.tsx' target='_blank'><FaGithub /></a></p>
+    <Giscus
+      id="comments"
+      repo="frederic-lang/awesome-konva-react-demos"
+      repoId="R_kgDOKtUG1g"
+      category="Announcements"
+      categoryId="DIC_kwDOKtUG1s4Cbi04"
+      mapping="pathname"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light"
+      lang="en"
+      loading="lazy"
+    />
     </>
   );
 };
